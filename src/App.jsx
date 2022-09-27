368ad7bd5d489cabce20b535a0d0709156951e0e
fahaddevs <fahadbinfaiz9999@gmail.com>
2022-09-27 22:52:01 +0600

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 
 function App() {
-
   return (
     <div className="App">
-      
+      <h2 className='app-title'>Todo App</h2>
+      <form>
+        <input type="text" className='form-control' placeholder='Task' />
+        <button type='submit' className='btn'>Add Item</button>
+      </form>
+
+      {/* <div className="items-wrapper">
+        <div className="item">
+          <p className='item-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <button type='button' className='remove-item'>x</button>
+        </div>
+      </div> */}
     </div>
   )
 }
